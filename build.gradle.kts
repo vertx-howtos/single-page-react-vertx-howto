0cde1211e54d34de9f30c4368ade12106bf51dc9
Thomas Segismont <tsegismo@redhat.com>
2019-06-06 00:14:35 +0200

    Added node gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.moowork.gradle.node.npm.NpmTask
+
 plugins {
   java
   application
+  id("com.moowork.node") version "1.3.1"
 }
 
 repositories {
@@ -14,6 +17,28 @@ dependencies {
 
 application {
   mainClassName = "io.vertx.howtos.react.BackendVerticle"
+}
+
+node {
+  version = "10.15.3"
+  npmVersion = "6.4.1"
+  download = true
+  nodeModulesDir = File("src/main/frontend")
+}
+
+val buildFrontend by tasks.creating(NpmTask::class) {
+  setArgs(listOf("run", "build"))
+  dependsOn("npmInstall")
+}
+
+val copyToWebRoot by tasks.creating(Copy::class) {
+  from("src/main/frontend/build")
+  destinationDir = File("${buildDir}/classes/java/main/webroot")
+  dependsOn(buildFrontend)
+}
+
+val processResources by tasks.getting(ProcessResources::class) {
+  dependsOn(copyToWebRoot)
 }
 
 tasks.wrapper {
