e357ca7b6f3cf68c012b2a20df98fec1a6252d4f
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-10-25 17:13:44 +0200

    add React Router

--- src/main/frontend/src/components/App.tsx
+++ src/main/frontend/src/components/App.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import logo from '../logo.svg';
 import './App.css';
 import Greeter from './Greeter';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
           Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-          Learn React
-      </a>
-      <Greeter />
-    </header>
-  </div>
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+            Learn React
+        </a>
+        <Greeter />
+      </header>
+    </div>
+  </Router>
 );
 
 export default App;
